5877e7204cb671e6a21941bdb72460deb382f952
Aleksandar Simic <asimic@gmail.com>
2014-01-10 22:58:11 +0000

    Added comments

--- src/postcoder/core.clj
+++ src/postcoder/core.clj
@@ -4,30 +4,39 @@
 
 (def api-server "http://uk-postcodes.com")
 
-(defn add-format [& format]
-  (let [suffix (first (flatten format))
-        json (str "json")]
+(defn- add-format
+  "append suffix to the URL"
+  [& format]
+  (let [suffix (first (flatten format))]
       (if (string? suffix)
         suffix
-        json)))
+        "json")))
 
-(defn- process-body [body suffix]
+(defn- process-body
+  "Return the body of the result based on the suffix"
+  [body suffix]
   (if (= suffix "xml")
     body
     (slurp body)))
 
-(defn- fetch-data [path suffix]
+(defn- fetch-data
+  [path suffix]
   (let [url (string/join "." [path suffix])
         result (http/get url)]
     (process-body (:body @result) suffix)))
 
-(defn lookup [postcode & format]
+(defn lookup
+  "Query a postcode with the option of getting the results in JSON,
+  XML, CSV or RDF. If none is provided, JSON is the default."
+  [postcode & format]
   (let [code (string/upper-case (string/replace postcode #"\s+" ""))
         path (string/join "/" [api-server "postcode" code])
         suffix (add-format format)]
     (fetch-data path suffix)))
 
-(defn data-at-point [lat lng & format]
+(defn data-at-point
+  "Return information at given latitude & longitude coordinates."
+  [lat lng & format]
   (let [point (string/join "," [lat lng])
         path (string/join "/" [api-server "latlng" point])
         suffix (add-format format)]
