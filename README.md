223991fe672096358a5240b031dba93d0461f48d
Aleksandar Simic <asimic@gmail.com>
2014-01-10 22:34:54 +0000

    data-at-point function added
    Used for retrieving data at latitude & longitude coordinates

--- README.md
+++ README.md
@@ -10,7 +10,24 @@ A Clojure client for [UK Postcodes](http://www.uk-postcodes.com/) API, an [ODI](
 
 ```clojure
 (require '[postcoder.core as postcode])
+(postcode/lookup "SW1A 1AA")
+```
+
+By default it returns data in JSON, but you can get it in any of the
+following formats as well: XML, CSV & RDF, like so:
+
+```clojure
 (postcode/lookup "SW1A 1AA" "json")
+(postcode/lookup "SW1A 1AA" "xml")
+(postcode/lookup "SW1A 1AA" "csv")
+(postcode/lookup "SW1A 1AA" "rdf")
+```
+
+### Get data for the nearest postcode at coordinates
+
+```clojure
+(require '[postcoder.core as postcode])
+(postcode/data-at-point "51.5010091744138" "-0.14157319687256223")
 ```
 
 ## License
--- src/postcoder/core.clj
+++ src/postcoder/core.clj
@@ -11,13 +11,24 @@
         suffix
         json)))
 
+(defn- process-body [body suffix]
+  (if (= suffix "xml")
+    body
+    (slurp body)))
+
+(defn- fetch-data [path suffix]
+  (let [url (string/join "." [path suffix])
+        result (http/get url)]
+    (process-body (:body @result) suffix)))
+
 (defn lookup [postcode & format]
   (let [code (string/upper-case (string/replace postcode #"\s+" ""))
         path (string/join "/" [api-server "postcode" code])
-        suffix (add-format format)
-        url (string/join "." [path suffix])
-        result (http/get url)
-        body (:body @result)]
-    (if (= suffix "xml")
-      body
-      (slurp body))))
+        suffix (add-format format)]
+    (fetch-data path suffix)))
+
+(defn data-at-point [lat lng & format]
+  (let [point (string/join "," [lat lng])
+        path (string/join "/" [api-server "latlng" point])
+        suffix (add-format format)]
+    (fetch-data path suffix)))
